2614d48ecefc6f1addd309d1f819a0aa07b7b873
Vivek Gawande <vivek040997@gmail.com>
2018-08-08 18:29:36 +0530

    Add Storage functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { IonicStorageModule } from '@ionic/storage';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
@@ -15,6 +15,7 @@ import { HomePage } from '../pages/home/home';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,27 +1,45 @@
 import { Component } from "@angular/core";
 import { NavController } from "ionic-angular";
 import { AlertController } from "ionic-angular";
+import { Storage } from "@ionic/storage";
 
 @Component({
   selector: "page-home",
   templateUrl: "home.html"
 })
 export class HomePage {
-  public tasks = [];
+  public tasks = [{ name: "Vivek" }];
 
   constructor(
     public navCtrl: NavController,
-    public alertCtrl: AlertController
-  ) {}
-
-  addTask(taskName) {
-    this.tasks.push({
-      name: taskName
+    public alertCtrl: AlertController,
+    public storage: Storage
+  ) {
+    storage.get("tasks").then(data => {
+      if (data === null) {
+        // Variable is not set in storage so init to blank
+        this.tasks = [];
+        storage.set("tasks", this.tasks);
+      } else {
+        // Get previous tasks from storage
+        this.tasks = data;
+      }
     });
   }
 
+  addTask(taskName) {
+    // Add a task with name taskName
+    this.tasks.push({
+      name: taskName
+    });
+    // Set the tasks variable in storage for persistency
+    this.storage.set("tasks", this.tasks);
+  }
+
   deleteTask(index) {
+    // Remove task at index
     this.tasks.splice(index, 1);
+    this.storage.set("tasks", this.tasks);
   }
 
   showPrompt() {
@@ -44,6 +62,7 @@ export class HomePage {
         {
           text: "Save",
           handler: data => {
+            // Call the addTask method
             this.addTask(data.title);
           }
         }
